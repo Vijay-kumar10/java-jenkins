9d921b4ea6549e74f3e5b1a7e3412ab60eb811db
Vijay-kumar10 <kumar888985@gmail.com>
2025-08-24 23:17:04 +0530

    second commit

--- First.java
+++ First.java
@@ -5,5 +5,7 @@ public class First {
     public static void main(String[] args) {
         System.out.println("This is first jenkin programm.");
         System.out.println("Today date is : "+new Date());
+        System.out.println("hello");
+        System.out.println("Added one more file");
     }
 }
