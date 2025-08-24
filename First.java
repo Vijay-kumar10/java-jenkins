004dca83e74fd3661570a83082924e49172f84bc
Vijay-kumar10 <kumar888985@gmail.com>
2025-08-24 22:59:46 +0530

    first commit

---
+++ First.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class First {
+
+    public static void main(String[] args) {
+        System.out.println("This is first jenkin programm.");
+        System.out.println("Today date is : "+new Date());
+    }
+}
